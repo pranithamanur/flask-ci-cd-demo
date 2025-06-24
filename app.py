c33f0321b02d3465ff5e3b334d3d907029d2c719
Pranitha <pranithamanur@gmail.com>
2025-06-24 18:32:39 +0530

    CI/CD test update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from CI/CD Pipeline!</h1>"
+    return "<h1>Updated via CI/CD!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
